aab7c3cb1e7ec6c3e8caf5b9553aba60f915b592
dyonvanraaij <I496295@fhict.nl>
2024-05-31 14:41:30 +0200

    aanpassingen

---
+++ src/components/Base/BaseDatePicker.vue
@@ -0,0 +1,39 @@
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  label: {
+    type: String,
+    required: true
+  },
+  help: {
+    type: String,
+    default: "",
+    required: false
+  },
+  placeholder: {
+    type: String,
+    default: "",
+    required: false
+  }
+})
+
+</script>
+
+<template>
+  <FormKit
+      type="datepicker"
+      :name="name"
+      :label="label"
+      :placeholder="placeholder"
+      :help="help"
+      overlay
+      popover
+  />
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup>
-import BaseButton from "@/components/BaseButton.vue";
-import BaseInput from "@/components/BaseInput.vue";
-import BaseSelect from "@/components/BaseSelect.vue";
+import BaseButton from "@/components/Base/BaseButton.vue";
+import TheHoursRegistration from "@/components/TheHoursRegistration.vue";
+import BaseDatePicker from "@/components/Base/BaseDatePicker.vue";
 </script>
 
 <template>
@@ -9,22 +9,18 @@ import BaseSelect from "@/components/BaseSelect.vue";
     <FormKit type="form" #default="{ value }" :actions="false">
 
     <div class="row">
-      <div class="col-12 p-4">
-        <div class="p-4">
+      <div class="col-2 p-4">
           <BaseButton primairy-color="#54E608" secondairy-color="#54e6081a">Create</BaseButton>
-        </div>
-        <div class="p-4">
+      </div>
+      <div class="col-2 p-4">
           <BaseButton primairy-color="#EB5252" secondairy-color="#eb52521a">Cancel</BaseButton>
-        </div>
       </div>
-
-      <div class="col-12">
-        <BaseInput label="Prijs" name="prijs" placeholder="10.00" />
-      </div>
-      <div class="col-12">
-        <BaseSelect :options="[1,2,3,4]" label="Eenheid" name="eenheid" />
+      <div class="col-4 p-4">
+        <BaseDatePicker label="Datum" name="date"/>
       </div>
     </div>
+
+     <TheHoursRegistration />
     </FormKit>
 
   </main>
